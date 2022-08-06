5086e684798410386055aa0071e6a2a54d46037a
Rafeh Qazi <qazi@cleverprogrammer.com>
2022-08-06 12:29:58 -0700

    Create Metabase.js

---
+++ Metabase.js
@@ -0,0 +1,7 @@
+<iframe
+    src="https://cleverprogrammer.metabaseapp.com/public/question/c4fbf8aa-3a2b-45cf-8b1b-80fba5782e55"
+    frameborder="0"
+    width="800"
+    height="600"
+    allowtransparency
+></iframe>
