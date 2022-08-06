a85ccfe6b134c59643f1a4f0c412b45964845974
Rafeh Qazi <qazi@cleverprogrammer.com>
2022-08-06 13:27:48 -0700

    Update Metabase.js

--- Metabase.js
+++ Metabase.js
@@ -1,18 +1,6 @@
-<div style="height:100px">Our element that prevents navigation</div>
+const element = document.querySelector('div');
 
-<script>
-    const element = document.querySelector('div');
-
-    element.addEventListener('touchstart', (e) => {
-        // prevent swipe to navigate gesture
-        e.preventDefault();
-    });
-
-<iframe
-    src="https://cleverprogrammer.metabaseapp.com/public/question/c4fbf8aa-3a2b-45cf-8b1b-80fba5782e55"
-    frameborder="0"
-    width="800"
-    height="600"
-    allowtransparency
-></iframe>
-</script>
+element.addEventListener('touchstart', (e) => {
+    // prevent swipe to navigate gesture
+    e.preventDefault();
+});
